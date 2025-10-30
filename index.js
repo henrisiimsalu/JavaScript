4aaa49f1ae46aa149df9a841a09e4f9fb4891939
Student <student@Administrators-Mac-mini.local>
2025-10-30 08:58:29 +0200

    test

--- index.js
+++ index.js
@@ -3,18 +3,18 @@
 const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const incereaseBtn = document.getElementById("increaseBtn");
-const countLabel = document.getElementById ("countLabel");
+const countLabel = document.getElementById("countLabel");
 let count = 0;
 
-incereaseBtn.onclick = function(){
-    count++;
-    countLabel.textContent = count;
-}
-decreaseBtn.onclick = function(){
-    count--;
-    countLabel.textContent = count
-}
-resetBtn.onclick = function(){
-    count = 0;
-    countLabel.textContent = count;
-}
+incereaseBtn.onclick = function () {
+  count++;
+  countLabel.textContent = count;
+};
+decreaseBtn.onclick = function () {
+  count--;
+  countLabel.textContent = count;
+};
+resetBtn.onclick = function () {
+  count = 0;
+  countLabel.textContent = count;
+};
